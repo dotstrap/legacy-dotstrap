994653a912013afee0bc149daa967dbfb024d262
mkwmms <mkwmms@users.noreply.github.com>
2015-09-18 11:35:16 -0600

    Add list command

--- lib/reel/bundle.rb
+++ lib/reel/bundle.rb
@@ -8,8 +8,8 @@ module Reel
     attr_accessor :repos
 
     # TODO: add progress bar https://github.com/grosser/parallel#progress--eta
-    def initialize(repos)
-      @repos = repos
+    def initialize(repos = nil)
+      @repos = repos unless repos.nil?
     end
 
     # FIXME: which parameter takes presidence when both are optional and one is
@@ -32,12 +32,38 @@ module Reel
       end
     end
 
+    def list(repos = @repos)
+      if repos.nil? || repos == 0 || repos.empty?
+        Dir.glob(File.join(Reel.reel_config_home, '**')).each do |repo_path|
+          next unless repo_path.include?('-') && File.directory?(repo_path)
+          repo = File.basename(repo_path)
+          repo[repo.index('-')] = '/'
+          puts repo
+          puts Reel::Git.new(repo).url
+          puts repo_path
+          puts "\n"
+        end
+      else
+        repos.each do |repo|
+          puts repo
+          bundle = Reel::Git.new(repo)
+          puts bundle.url
+          repo_path = bundle.repo_path
+          puts repo_path
+          # shell = Reel::Shell.new(repo_path)
+          # TODO: only output all associated files on --verbose
+          # shell.list(repo_path)
+          puts "\n"
+        end
+      end
+    end
+
     def load_configs(repo_path)
       shell = ''
       repo_path.each do |r|
         shell = Reel::Shell.new(r)
         shell.configure(repo_path)
-        # TODO: cleanup CLI output
+        # TODO: cleanup CLI output; only output `make sure to source...` once
         puts "Make sure to `source \"#{shell.reel_config_file}\"` in your shell " \
           "startup file" unless shell.repo_config_files.empty?
       end
